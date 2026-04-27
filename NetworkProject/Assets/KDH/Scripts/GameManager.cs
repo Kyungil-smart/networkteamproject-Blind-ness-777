4bcc0e3e0594e2066cfcc171717adc887729fdf5
Crowasang <bighyun620@naver.com>
2026-04-27 10:51:49 +0900

    주석 추가

--- NetworkProject/Assets/KDH/Scripts/GameManager.cs
+++ NetworkProject/Assets/KDH/Scripts/GameManager.cs
@@ -6,9 +6,9 @@ using Unity.Netcode;
 public class GameManager : NetworkBehaviour
 {
     public static GameManager Instance {get; private set;}
-    public NetworkVariable<GamePhase> CurrentPhase = new(GamePhase.Waiting);
-    public NetworkVariable<int> AlivePlayer = new(0);
-    [SerializeField] public float _movingTime = 10f;
+    public NetworkVariable<GamePhase> CurrentPhase = new(GamePhase.Waiting);    // 게임 페이즈 변수
+    public NetworkVariable<int> AlivePlayer = new(0);                           // 플레이 인원수 변수
+    [SerializeField] public float _movingTime = 10f;                            // 숨는 시간 변수
     private void Awake()
     {
         if (Instance == null)
@@ -23,6 +23,7 @@ public class GameManager : NetworkBehaviour
         }
     }
 
+    // 게임 시작 함수
     public void StartGame()
     {
         if (!IsServer) return;
@@ -30,6 +31,7 @@ public class GameManager : NetworkBehaviour
         StartCoroutine(GamePlay());
     }
 
+    // 플레이어 죽을때마다 인원 수 감소
     public void OnPlayerDead()
     {
         if (!IsServer) return;
@@ -40,6 +42,7 @@ public class GameManager : NetworkBehaviour
             CurrentPhase.Value = GamePhase.GameOver;
     }
 
+    // 숨는 페이즈에서 총쏘는 페이즈로 갔을 시 호출
     public void ShootingPhase()
     {
         if (!IsServer) return;
@@ -48,6 +51,7 @@ public class GameManager : NetworkBehaviour
         CurrentPhase.Value = GamePhase.HideAndSeek;
     }
 
+    // 게임 플레이 루틴
     public IEnumerator GamePlay()
     {
         while (AlivePlayer.Value > 1)
