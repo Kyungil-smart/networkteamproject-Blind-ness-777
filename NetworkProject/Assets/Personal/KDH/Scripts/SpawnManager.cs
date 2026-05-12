317359a912d0c6497a442e7b52c5dffb710a968f
Crowasang <bighyun620@naver.com>
2026-05-12 17:53:35 +0900

    오브젝트 안쪽소환안하는 컴포넌트추가

--- NetworkProject/Assets/Personal/KDH/Scripts/SpawnManager.cs
+++ NetworkProject/Assets/Personal/KDH/Scripts/SpawnManager.cs
@@ -29,12 +29,8 @@ public class SpawnManager : MonoBehaviour
         foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
         {
             Vector3 _randomPoint = GetNavMeshPoint();
-            _randomPoint += Vector3.up * 1.0f;
             GameObject _player = Instantiate(_playerPrefab, _randomPoint, Quaternion.identity);
-            var controller = _player.GetComponent<CharacterController>();
-            if (controller != null) controller.enabled = false;
             _player.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId);
-            if (controller != null) controller.enabled = true;
         }
     }
     
@@ -43,7 +39,7 @@ public class SpawnManager : MonoBehaviour
         for (int i = 0; i < 30; i++)
         {
             Vector3 randomPoint = new Vector3(
-                Random.Range(-20f, 20f), 1.0f, Random.Range(-20f, 20f));
+                Random.Range(-20f, 20f), 0f, Random.Range(-20f, 20f));
         
             if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, 5f, NavMesh.AllAreas))
                 return hit.position;
